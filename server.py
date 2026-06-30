749485df1c8af064ba67317061099b599bbb2bad
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-30 09:51:27 +0000

    integrate model in flask app

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask,jsonify
+from model import Network
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello ,World</p>"
+pixel_array = np.zeros((784,1))
+
+NeuralNetwork = Network([784,50,30,10])
+
+
+@app.route('/', methods=['GET'])
+def take_input(pixel_input):
+    global pixel_array
+    pixel_array = np.array(pixel_input)
+    predicted_digit = {
+        "digit": NeuralNetwork.predict(pixel_array)
+    }
+    return jsonify({predicted_digit})
+    
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
