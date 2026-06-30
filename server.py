32dbd7ae09d02ecc9ceffb8097a9c7743f31e287
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-30 09:58:13 +0000

    add cors, post and other necessary methods in server to optimize it

--- server.py
+++ server.py
@@ -1,23 +1,29 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 from model import Network
+from flask_cors import CORS
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
-pixel_array = np.zeros((784,1))
 
 NeuralNetwork = Network([784,50,30,10])
 
 
-@app.route('/', methods=['GET'])
-def take_input(pixel_input):
-    global pixel_array
-    pixel_array = np.array(pixel_input)
-    predicted_digit = {
-        "digit": NeuralNetwork.predict(pixel_array)
-    }
-    return jsonify({predicted_digit})
+@app.route('/predict', methods=['POST'])
+def take_input():
+    data = request.get_json()
     
+    if not data or 'pixels' not in data:
+        return jsonify({"error": "No Pixel data provided"}), 400
+    
+    pixel_input = data['pixels']
+    pixel_array = np.array(pixel_input).reshape(784,1)
+    
+    network_output = NeuralNetwork.predict(pixel_array)
+    predicted_digit = int(network_output)
+    
+    return jsonify({"digit": predicted_digit})
     
 
 if __name__ == "__main__":
